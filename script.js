f088ec1d61b0c51887ee03ac5b0b71fee856ecaf
Henry Izuchukwu <izuchukwuhenry62@gmail.com>
2026-02-21 01:44:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ btn.addEventListener("click", () => {
     btn.textContent = "Read More";
   }
 });
+
+const form = document.querySelector("form");
+const modal = document.getElementById("successModal");
+const closeBtn = document.getElementById("closeModal");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // stop page refresh
+
+  modal.classList.add("active");
+  form.reset(); // clear form
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
